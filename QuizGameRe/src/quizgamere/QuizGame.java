21a7a674f3d7437f2d6808ad00aa691e0a11a7a7
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-10-09 08:01:40 +0800

    added loadQuestion() method in QuizGame.java

--- QuizGameRe/src/quizgamere/QuizGame.java
+++ QuizGameRe/src/quizgamere/QuizGame.java
@@ -1,8 +1,13 @@
 
 package quizgamere;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Question {
     String question;
@@ -37,8 +42,46 @@ class Question {
     public void shuffleAnswers() {
         Collections.shuffle(answers);
     }
+    
+    // for testing purposes
+    public void printQuestion() {
+        System.out.println(question);
+        String[] opt = {"A", "B", "C", "D"};
+        for (int i = 0; i < opt.length; i++) {
+            System.out.println(opt[i] + ". " + answers.get(i));
+        }
+    }
 } 
 
 public class QuizGame {
+    ArrayList<Question> setOfQuestions;
+    int score;
     
+    public QuizGame() {
+        setOfQuestions = new ArrayList<>();
+    }
+    
+    public void loadQuestion(String questionfile) {
+        String question = "";
+        ArrayList<String> answers = new ArrayList<>();
+        String correctAns = "";
+        
+        Scanner s;
+        try {
+            s = new Scanner(new File(questionfile), "UTF-8");
+            s.useDelimiter(",");
+            while(s.hasNext()) {
+                int i = 0;
+                while (i < 6) {
+                    if (i == 0) question = s.next();
+                    else if (i > 0 && i < 5) answers.add(s.next());
+                    else correctAns = s.next();   
+                    i++;
+                }        
+                setOfQuestions.add(new Question(question, answers, correctAns));
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(QuizGameRe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
